451598bf83b4bc67f9113eb34d0e8f30f4873a3d
ElvBC <elvin@elvins-air.speedport_w_724v_09011603_00_009>
2015-05-19 23:41:16 +0200

    LotteryProgramm
    1 Function of the Lottery Programm

---
+++ LotteryProgram/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner eing = new Scanner(System.in);
+		int players,MenuSelection;
+		int  wholeNumbers[]=new int [49];
+		int playersNumbers[][]=new int [1000][6]; //Max. 1000 Players
+		String playerFirstName[]= new String [50]; //Max 50. Chars
+		String playerLastName[]= new String [50]; //Max. 50 Chars
+
+		while(true){
+			System.out.println("=== Lottery Menu ==="); //Menu
+			System.out.println("1 Enter ticket\n2 Lottery drawing\n3 Show result\n0 Quit\nPlease enter (0-3):");
+			MenuSelection=eing.nextInt();
+			switch(MenuSelection){
+			case 0://Quit
+			System.exit(0);
+			break; 
+			
+			case 1:
+			System.out.println("How many are playing this game: ");
+			players=eing.nextInt();
+			for (int i = 0; i <players; i++) {
+				eing.nextLine();
+				System.out.println("Give in your First Name: ");
+				playerFirstName[i]=eing.nextLine();
+				System.out.println("Give in your Last Name: ");
+				playerLastName[i]=eing.nextLine();//First & LastName
+				for (int j = 0; j <6; j++){
+					System.out.println(j+1+". Number");
+					playersNumbers[i][j]=eing.nextInt(); //On place i give him number j 
+				}//for2
+				System.out.println("\n");
+			}//for
+			break;
+			
+			case 2:
+			break;
+			
+			case 3:
+			break;
+			
+			default: //Wrong Input
+			System.out.println(MenuSelection +"is a not allowed input");
+			break;
+			}//switch
+		}//while
+	}//public static void 
+}//Main
