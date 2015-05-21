f89ba7f54ff7fec62c4ffe39055677dd6a5bb6b2
Elvin <ElvBC@users.noreply.github.com>
2015-05-21 11:12:21 +0200

    Update Main.java
    Final program.
    Going to test it now.

--- LotteryProgramm/Main.java
+++ LotteryProgramm/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -12,52 +13,52 @@ public class Main {
 		String playerLastName[]= new String [50]; //Max. 50 Chars
 
 		while(true){
-		System.out.println("=== Lottery Menu ==="); //Menu
+			System.out.println("=== Lottery Menu ==="); //Menu
 			System.out.println("1 Enter ticket\n2 Lottery drawing\n3 Show result\n0 Quit\nPlease enter (0-3):");
 			MenuSelection=eing.nextInt();
 			switch(MenuSelection){
 //Case0--------------------------------------------------------------------------------------------------------------
-		case 0:
-		System.exit(0);
-		break; //Quit
-
-//Case1--------------------------------------------------------------------------------------------------------------	
-		case 1:
-		System.out.println("How many are playing this game: ");
-		players=eing.nextInt();
-		for (int i = 0; i <players; i++) {
-			eing.nextLine();
-			System.out.println("Give in your First Name: ");
-			playerFirstName[i]=eing.nextLine();
-			System.out.println("Give in your Last Name: ");
-			playerLastName[i]=eing.nextLine();//First & LastName
-			for (int j = 0; j <6; j++){
-				System.out.println(j+1+". Number");
-		//----------------------ERROR HANDLING----------------------START
-				if((playersNumbers[i][j] = eing.nextInt())>49){//INPUT: Give player i the number j
-				System.out.println("Only numbers >0 and <50 are allowed."); 
-				j--;
-				continue;
-				}else{
-				if((playersNumbers[i][j])<1){//Give player i the number j
-				System.out.println("Only numbers >0 and <50 are allowed."); 
-				j--;
-				continue;
-			}
-			for (int a = j-1; a >=0; a--) {
-				if(playersNumbers[i][j]==playersNumbers[i][a]){
-				System.out.println("You cant give in the same number twice.");
-				j--;
-				}//if
-			}//for
-			}//else
-		}//for2
-		//----------------------ERROR HANDLING----------------------END
-		System.out.println("\n");
-	}//for
-			break; //Adding player + his numbers
+			case 0:
+			System.exit(0);
+			break; //Quit
 			
-//Case2--------------------------------------------------------------------------------------------------------------
+//Case1--------------------------------------------------------------------------------------------------------------	
+			case 1:
+			System.out.println("How many are playing this game: ");
+			players=eing.nextInt();
+			for (int i = 0; i <players; i++) {
+				eing.nextLine();
+				System.out.println("Give in your First Name: ");
+				playerFirstName[i]=eing.nextLine();
+				System.out.println("Give in your Last Name: ");
+				playerLastName[i]=eing.nextLine();//First & LastName
+				for (int j = 0; j <6; j++){
+					System.out.println(j+1+". Number");
+				//----------------------ERROR HANDLING----------------------START
+					if((playersNumbers[i][j] = eing.nextInt())>49){//INPUT: Give player i the number j
+						System.out.println("Only numbers >0 and <50 are allowed."); 
+						j--;
+						continue;
+					}else{
+					if((playersNumbers[i][j])<1){//Give player i the number j
+						System.out.println("Only numbers >0 and <50 are allowed."); 
+						j--;
+						continue;
+					}
+					for (int a = j-1; a >=0; a--) {
+						if(playersNumbers[i][j]==playersNumbers[i][a]){
+							System.out.println("You cant give in the same number twice.");
+							j--;
+						}//if
+					}//for
+					}//else
+				}//for2
+				//----------------------ERROR HANDLING----------------------END
+				System.out.println("\n");
+			}//for
+			break; //Adding player + his numbers
+
+//Case2--------------------------------------------------------------------------------------------------------------			
 			case 2:
 			for (int i = 0; i < 6; i++){
 			lotteryNumbers[i]=(int) ((Math.random()*49)+1);
@@ -77,7 +78,8 @@ public class Main {
 			System.out.println(""); //Newline
 			tmp=1;
 			break;
-
+			
+			
 //Case3--------------------------------------------------------------------------------------------------------------
 			case 3:
 			if(tmp==1){
@@ -99,11 +101,10 @@ public class Main {
 			System.out.println("");
 			}else System.out.println("Please first press 2 to draw the numbers.");
 			break;
-
-
-//Default------------------------------------------------------------------------------------------------------------
+			
+//Default------------------------------------------------------------------------------------------------------------			
 			default: //Wrong Input
-			System.out.println(MenuSelection +"is a not allowed input");
+			System.out.println(MenuSelection +" is a not allowed input\n");
 			break;
 			}//switch
 		}//while
